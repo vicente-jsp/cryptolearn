79fd041abf4e0500ca9c7d7947bebc7eab5ec822
vicente-jsp <sheanv94@gmail.com>
2026-04-07 17:08:12 +0800

    sb

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -72,7 +72,7 @@ export default function AdminLayout({
     return (
         <div className=" flex bg-slate-50 dark:bg-gray-900 transition-colors duration-300 overflow-hidden">
             {/* --- FIXED SIDEBAR (250px) --- */}
-            <aside className="w-[250px] flex-shrink-0 bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 p-4 flex flex-col h-[100%]}">
+            <aside className="w-[250px] flex-shrink-0 bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 p-4 flex flex-col h-[120%]}">
                 
                 <h1 className="text-2xl font-extrabold text-indigo-600 dark:text-indigo-400 mb-8">
                     Admin
